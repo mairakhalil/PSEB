8ad1c92d8b63806aa745ff77432c48b19fc80795
mairakhalil <mairakhalil73@gmail.com>
2022-02-11 17:02:01 +0500

    PSEB

---
+++ js.js/js.js
@@ -0,0 +1,19 @@
+//SLIDER FUNCTION!
+// var myIndex = 0;
+// sslider();
+// function sslider()
+// {
+//     var i;
+//     var x = document.getElementsByClassName("images");
+//     for(i = 0; i < x.length; i++)
+//     {
+//         x[i].style.display = "none";  
+//     }
+//     myIndex++;
+//     if(myIndex > x.length) 
+//     {
+//         myIndex = 1
+//     }    
+//     x[myIndex-1].style.display = "block";  
+//     setTimeout(sslider, 3000); //Change image every 3 seconds
+// }
